9c42c3fee7010a7e819bd86c6b26cc64c334fdbb
KaruriCreations <vincent.maina3@student.moringaschool.com>
2026-03-30 13:44:35 +0300

    used rest and spread syntax to add logic

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+require(`datejs`)
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+    merge_date: new Date().toString("M/d/yyyy") 
+  };
 
+  args.forEach(arg => {
+    if (Array.isArray(arg)) {
+      combinedObject.users.push(...arg);
+    }
+  });
 
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
